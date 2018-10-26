136c6c62e2790f350dea6545176616e82f53fe14
dev027 <steve66web+github@gmail.com>
2018-10-26 15:34:53 +0100

    Added ObjectExtensions.GetValue

---
+++ Utilities/Extensions/ObjectExtensions/GetValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace Utilities.Extensions.ObjectExtensions
+{
+    // ReSharper disable once PartialTypeWithSinglePart
+    public static partial class ObjectExtensions
+    {
+        public static T GetValue<T>(this object obj, string propertyName)
+        {
+            PropertyInfo propertyInfo = obj.GetType().GetProperty(propertyName);
+            if (propertyInfo == null)
+            {
+                throw new ArgumentException($"Property {propertyName} does not exist",
+                    nameof(propertyName));
+            }
+            return (T)propertyInfo.GetValue(obj);
+        }
+    }
+}
